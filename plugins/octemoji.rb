7d8c738dd1319d581fdaad71634aa0f4aa601b06
Aaron Huang <aaroms9733@gmail.com>
2013-01-15 17:27:08 +0800

    Add emoji tag

--- plugins/octemoji.rb
+++ plugins/octemoji.rb
@@ -5,11 +5,21 @@
 # {% emoji smile %}
 
 module Jekyll
+  @emoji = ''
   class EmojiTag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
+      if markup =~ /([a-z0-9\+\-_]+)/
+        @emoji = $1
+      end
+      super
     end
     
     def render(context)
+      if @emoji
+        "<i class='emoji #{@emoji}'></i>"        
+      else
+        "Error processing input, expected syntax: {% emoji emoji_name %}"
+      end
     end
   end
 end
